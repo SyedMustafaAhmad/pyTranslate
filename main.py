5786c45a5dfa00721bb8f6ef78e1865f6d17e005
Syed Mustafa Ahmad <syedmustafaahmad@gmail.com>
2024-03-22 13:01:07 +0100

    Updated main.py

--- main.py
+++ main.py
@@ -18,21 +18,27 @@ def getText():
     image_text = pytesseract.image_to_string(image)
     return image_text
 
+def processText(orignal_text):
+    return orignal_text.replace('\n', ' ')
+
 def translateText(to_translate):
     translated = GoogleTranslator(source=TRANSLATE_FROM, target=TRANSLATE_TO).translate(to_translate)
     return translated
 
-def saveTranslation(text):
+def saveTranslation(orignal_text, translated_text):
     time_str = time.strftime("%Y%m%d-%H%M%S")
     f = open(SAVE_TO + time_str + '.txt', "w")
-    f.write(text)
+    f.write(translated_text)
+    f.write("\n\n" + '-'*10 + "\n\n")
+    f.write(orignal_text)
     f.close()
     return time_str
 
 def openFile(file_name):
     subprocess.call([OPEN_WITH, SAVE_TO + file_name + '.txt'])
 
-to_translate = getText()
-translated = translateText(to_translate)
-file_name = saveTranslation(translated)
+orignal_text = getText()
+to_translate = processText(orignal_text)
+translated_text = translateText(to_translate)
+file_name = saveTranslation(to_translate, translated_text)
 openFile(file_name)
